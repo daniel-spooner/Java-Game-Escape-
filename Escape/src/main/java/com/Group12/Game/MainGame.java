3e88b6e2b316fb454faa8de4c5d76d224d8996dd
Wilson <wwa87@sfu.ca>
2020-03-05 20:01:21 -0800

    Adding Methods

--- Escape/src/main/java/com/Group12/Game/MainGame.java
+++ Escape/src/main/java/com/Group12/Game/MainGame.java
@@ -1,8 +1,39 @@
 package com.Group12.Game;
 
 public class MainGame {
+	
+	public void startGame() {
+		
+	}
+	
+	public void update() {
+		
+		
+	}
+	
+	private void updatePlayer() {
+		
+		
+	}
+	private void updateEnemies() {
+		
+		
+	}
+	
+	private void updateDisplay() {
+		
+		
+	}
+	
+	private void makeBoard(String x) {
+		
+		
+	}
     public static void main( String[] args )
     {
-        System.out.println( "Hello World1111!" );
+    	
+    	
+    	
+
     }
 }
