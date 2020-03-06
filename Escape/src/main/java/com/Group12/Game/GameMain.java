95395fa249b2746bdb3e5003676d040010584c2f
nta37 <nta37@sfu.ca>
2020-03-05 20:42:30 -0800

    GameMain outline update

--- Escape/src/main/java/com/Group12/Game/GameMain.java
+++ Escape/src/main/java/com/Group12/Game/GameMain.java
@@ -2,35 +2,51 @@ package com.Group12.Game;
 
 public class GameMain {
 	
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!111111" );
-    } 
+	// Attributes
+	
+	private static GameMain gameMain = new GameMain();
+	
+	private Enemy[] enemies = new Enemy[];
+	private Collectible[] collectibles = new Collectible[];
+	private MainCharacter[] mainChar = new MainCharacter;
+	private Board board = new Board;
+	
+	private int objectivesRemaining;
+	private StateType gameState;
+	private int score;
+	private TickTimer tick = new TickTimer;
+	
+	// Methods
+	
+	private GameMain() {
+		
+	}
+	
+	public static GameMain getInstance() {
+		
+	}
+	
 	public void startGame() {
 		
 	}
 	
 	public void update() {
 		
-		
 	}
 	
 	private void updatePlayer() {
 		
-		
 	}
+	
 	private void updateEnemies() {
-		
 		
 	}
 	
 	private void updateDisplay() {
 		
-		
 	}
 	
 	private void makeBoard(String x) {
-		
 		
 	}
 
