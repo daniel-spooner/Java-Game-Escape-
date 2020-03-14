c1da2b9132a2f0afa451cfaee0e7e1c3e23089ea
nta37 <nta37@sfu.ca>
2020-03-14 16:24:57 -0700

    makeBoard initial build

--- Escape/src/main/java/com/Group12/Game/GameMain.java
+++ Escape/src/main/java/com/Group12/Game/GameMain.java
@@ -15,7 +15,7 @@ public class GameMain {
 	
 	private Enemy[] enemies;
 	private Collectible[] collectibles;
-	private MainCharacter[] mainChar;
+	private MainCharacter mainChar;
 	private Board board;
 	
 	private int objectivesRemaining;
@@ -84,10 +84,11 @@ public class GameMain {
 		ySize = sc.nextInt();
 		board.setXSize(xSize);		//TODO: tell Daniel board either must be dynamic or accept dims in constructor
 		board.setYSize(ySize);
+		sc.nextLine();
 		
 		/* characters in file:
 		 * 	. = Open
-		 * 	# = Barrier	TODO: tell Daniel celltype is Barrier in the UML Diagram
+		 * 	# = Barrier				TODO: tell Daniel celltype is Barrier in the UML Diagram
 		 * all other characters also on Open tiles
 		 * 	@ = MainCharacter
 		 * 	G = Goal
@@ -97,7 +98,42 @@ public class GameMain {
 		 * 	W = WeaponCollectible
 		 */
 		
-		
+		for (int i = 0; i < ySize; i++) {
+			String line = sc.nextLine();	//TODO: determine if file must end with newline character (currently does not)
+			for (int j = 0; j < xSize; j++) {
+				char c = line.charAt(j);
+				switch (c) {
+				case '#':
+					this.board.setCellType(j, i, cellType.BARRIER);
+					break;
+				case '@':
+					this.mainChar.setX(j);	//TODO: wasn't this supposed to be setPos() from the diagram?
+					this.mainChar.setY(i);
+					break;
+				case 'G':	//TODO: turns out knowing where the goal is is important for game logic; will add somehow - return value or private attr?
+					break;
+				case 'E':	//TODO: array length is fixed on creation, so enemies/collectibles need drastic rework (i recommend java.util.ArrayList)
+					Enemy temp = new Enemy(j,i);
+					this.enemies.add(temp);
+					break;
+				case 'P':
+					Punishment temp = new Punishment(j,i);
+					this.collectibles.add(temp);
+					break;
+				case 'O':
+					ObjectiveReward temp = new ObjectiveReward(j,i);
+					this.collectibles.add(temp);
+					break;
+				case 'W':
+					WeaponCollectible temp = new WeaponCollectible(j,i);
+					this.collectibles.add(temp);
+					break;
+				case '.':	default:
+					//board.setCellType(j, i, cellType.OPEN);	// at time of writing, cells initialized as OPEN
+					break;
+				}
+			}
+		}
 		
 		sc.close();
 	}
