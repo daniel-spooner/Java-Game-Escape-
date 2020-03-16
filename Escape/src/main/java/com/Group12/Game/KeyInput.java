e3114afb42d822e456117226845528c956d1d894
wwa87 <wilson_wu_2@sfu.ca>
2020-03-15 21:43:20 -0700

    Update KeyInput.java

--- Escape/src/main/java/com/Group12/Game/KeyInput.java
+++ Escape/src/main/java/com/Group12/Game/KeyInput.java
@@ -1,53 +1,100 @@
 package com.Group12.Game;
+import java.awt.*;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
+import javax.swing.*;
+
 import java.awt.event.KeyAdapter;
 
 
-public class KeyInput extends KeyAdapter{
+public class KeyInput extends KeyAdapter implements KeyListener{
 	
-	BoardEntity character;
-	
-	public void keyInput(BoardEntity character) {
-		this.character = character;
-	}
-	public void keyPressed(KeyEvent e) {
-	int key = e.getKeyCode();
-	int newPos;
-	
+	BoardEntity character = new BoardEntity();
+	int x = character.getXPos();
+	int y = character.getXPos();
 
-	//up
-	if(key == KeyEvent.VK_W) {
-		newPos = character.getXPos() + 1;
-		character.setX(newPos);
+	
+	public void keyDemo(){
+
+		
+		JFrame frame = new JFrame();
+	    frame.setSize(300, 300);
+	    frame.setVisible(true);
+		frame.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// NO NEED
+			}
+
+			public void keyPressed(KeyEvent e) {
+				int key = e.getKeyCode();
+				int newPos;
+				
+
+					//up
+					if(key == KeyEvent.VK_W) {
+						newPos = character.getXPos() + 1;
+						character.setX(newPos);
+						System.out.println(newPos);
+					}
+					//left
+					else if(key == KeyEvent.VK_A) {
+						newPos = character.getYPos() - 1;
+						character.setY(newPos);
+						System.out.println(newPos);
+					}
+					//down
+					else if(key == KeyEvent.VK_S) {
+						newPos = character.getXPos() - 1;
+						character.setX(newPos);
+						System.out.println(newPos);
+					}//right
+					else if(key == KeyEvent.VK_D) {
+						newPos = character.getYPos() + 1;
+						character.setY(newPos);
+						System.out.println(newPos);
+					}
+					else if(key == KeyEvent.VK_LEFT) {
+						//shoot
+					}
+					else if(key == KeyEvent.VK_RIGHT) {
+						//shoot
+					}
+					else if(key == KeyEvent.VK_UP) {
+						//shoot
+					}
+					else if(key == KeyEvent.VK_DOWN) {
+						//shoot             
+					}
+				
+				}
+				
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// NO NEED
+				
+			} 
+		
+		
+	
+		
+		
+		
+		});
 	}
-	//left
-	else if(key == KeyEvent.VK_A) {
-		newPos = character.getYPos() - 1;
-		character.setY(newPos);
-	}
-	//down
-	else if(key == KeyEvent.VK_S) {
-		newPos = character.getXPos() - 1;
-		character.setX(newPos);
-	}//right
-	else if(key == KeyEvent.VK_D) {
-		newPos = character.getYPos() + 1;
-		character.setY(newPos);
-	}
-	else if(key == KeyEvent.VK_LEFT) {
-		//shoot
-	}
-	else if(key == KeyEvent.VK_RIGHT) {
-		//shoot
-	}
-	else if(key == KeyEvent.VK_UP) {
-		//shoot
-	}
-	else if(key == KeyEvent.VK_DOWN) {
-		//shoot             
-	}
+
+
+
+
+	public static void main(String[] args) {
+		KeyInput asdf = new KeyInput();
+		asdf.keyDemo();
+		System.out.println("hi");
+		
 	}
 
 
