02fee1c845e6e721d87ef4df87c6c8ae8320fe06
Wilson <wwa87@sfu.ca>
2020-03-15 21:40:42 -0700

    Testing boardentity

--- Escape/src/main/java/com/Group12/Game/BoardEntity.java
+++ Escape/src/main/java/com/Group12/Game/BoardEntity.java
@@ -1,5 +1,33 @@
 package com.Group12.Game;
 
-public class BoardEntity {
 
-}
+public class BoardEntity {	
+	
+	//X and Y position for Main Character
+
+	private int charXpos;
+	private int charYpos;
+	
+	
+	public int getXPos() {
+		return charXpos;
+	
+	}
+	
+	public int getYPos() {
+		return charYpos;
+	}
+	
+	public void setX(int x) {
+		this.charXpos = x;
+	}
+	
+	public void setY(int y) {
+		this.charYpos = y;
+	}
+
+
+	
+}	
+
+
