56de42964a419be527560830fe30cea19cfd1a92
Daniel Spooner <daniel_spooner@sfu.ca>
2020-03-15 23:04:24 -0700

    comments for Board and Cell

--- Escape/src/main/java/com/Group12/Game/Board.java
+++ Escape/src/main/java/com/Group12/Game/Board.java
@@ -1,5 +1,12 @@
 package com.Group12.Game;
 
+
+/**
+ * Represents the board where the game takes place
+ * @author Daniel
+ *
+ */
+
 public class Board{
 	
 	// Attributes
@@ -9,6 +16,7 @@ public class Board{
 	private Cell[][] boardMatrix;
 	
 	// Constructors
+	
 	public Board() {
 		this(25, 20, 30);
 	}
@@ -17,6 +25,12 @@ public class Board{
 		this(xSize, ySize, 30);
 	}
 	
+	/**
+	 * Constructor for the board object
+	 * @param xSize the size of the X dimension
+	 * @param ySize the size of the Y dimension
+	 * @param cellSize the pixel size of a single cell
+	 */
 	public Board(int xSize, int ySize, int cellSize) {
 		this.xSize = xSize;
 		this.ySize = ySize;
@@ -30,26 +44,72 @@ public class Board{
 	}
 	
 	// Public Methods
-	
+	/**
+	 * Getter for a specific cell in the board
+	 * @param x the x coordinate of the cell
+	 * @param y the y coordinate of the cell
+	 * @return The cell at the corresponding x and y coordinate
+	 */
 	public Cell getCell(int x, int y) {
 		return boardMatrix[x][y];
 	}
 	
+	/**
+	 * Getter for the type of a cell in the board
+	 * @param x the x coordinate of the cell
+	 * @param y the y coordinate of the cell
+	 * @return The type of the cell at the corresponding x and y coordinate
+	 */
 	public cellType getCellType(int x, int y) {
 		return boardMatrix[x][y].getType();
 	}
 	
+	/**
+	 * Setter for the type of a cell in the board
+	 * @param x the x coordinate of the cell
+	 * @param y the y coordinate of the cell
+	 * @param type the type that the cell is set to
+	 */
 	public void setCellType(int x, int y, cellType type) {
 		boardMatrix[x][y].setType(type);
 	}
 	
 	
+	/** 
+	 * Getter for the pixel size of a single cell in the board 
+	 * @return the size of a cell
+	 */
 	public int getCellSize() {return cellSize;}
+	
+	/** 
+	 * Getter for the X dimension of the board 
+	 * @return the X dimension of the board
+	 */
 	public int getXSize() {return xSize;}
+	
+	/** 
+	 * Getter for the Y dimension of the board 
+	 * @return the Y dimension of the board
+	 */
 	public int getYSize() {return ySize;}
 	
+	
+	/** 
+	 * Setter for the pixel size of a single cell in the board 
+	 * @param size the size to set each cell to
+	 */
 	public void setCellSize(int size) {cellSize = size;}
+	
+	/** 
+	 * Setter for the X dimension of the board
+	 * @param size the new X dimension of the board
+	 */
 	public void setXSize(int size) {xSize = size;}
+	
+	/** 
+	 * Setter for the Y dimension of the board
+	 * @param size the new Y dimension of the board
+	 */
 	public void setYSize(int size) {ySize = size;}
 	
 }
--- Escape/src/main/java/com/Group12/Game/Cell.java
+++ Escape/src/main/java/com/Group12/Game/Cell.java
@@ -2,27 +2,48 @@ package com.Group12.Game;
 
 enum cellType { Open, Wall }
 
+/**
+ * A class representing a unit in the game board
+ * @author Daniel
+ *
+ */
 public class Cell {
 	
 	// Attributes
+	
 	private cellType type;
 	
 	
 	// Constructors
+	/**
+	 * Creates a new Cell object with default state "Open"
+	 */
 	public Cell() {
 		this.type = cellType.Open;
 	}
 	
+	/**
+	 * Creates a new Cell object
+	 * @param type	The type of Cell that is to be created
+	 */
 	public Cell(cellType type) {
 		this.type = type;
 	}
 	
 	
 	// Public Methods
+	/**
+	 * getter for the type of the Cell
+	 * @return the type of the Cell
+	 */
 	public cellType getType() {
 		return this.type;
 	}
 	
+	/**
+	 * setter for the type of the Cell
+	 * @param type	The type to set the Cell to
+	 */
 	public void setType (cellType type) {
 		this.type = type;
 	}
