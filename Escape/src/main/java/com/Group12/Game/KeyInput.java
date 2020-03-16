87c40dfba14e8db3d9f412c94d6ef535be4fbe1f
wwa87 <wilson_wu_2@sfu.ca>
2020-03-15 18:38:47 -0700

    Updated KeyInput.java to handle user Key Inputs.

--- Escape/src/main/java/com/Group12/Game/KeyInput.java
+++ Escape/src/main/java/com/Group12/Game/KeyInput.java
@@ -7,15 +7,49 @@ import java.awt.event.KeyAdapter;
 
 public class KeyInput extends KeyAdapter{
 	
-	MainCharacter character;
+	BoardEntity character;
 	
-	public void keyInput(MainCharacter character) {
+	public void keyInput(BoardEntity character) {
 		this.character = character;
 	}
-
 	public void keyPressed(KeyEvent e) {
-		character.keyPressed(e);
-		
+	int key = e.getKeyCode();
+	int newPos;
+	
+
+	//up
+	if(key == KeyEvent.VK_W) {
+		newPos = character.getXPos() + 1;
+		character.setX(newPos);
 	}
+	//left
+	else if(key == KeyEvent.VK_A) {
+		newPos = character.getYPos() - 1;
+		character.setY(newPos);
+	}
+	//down
+	else if(key == KeyEvent.VK_S) {
+		newPos = character.getXPos() - 1;
+		character.setX(newPos);
+	}//right
+	else if(key == KeyEvent.VK_D) {
+		newPos = character.getYPos() + 1;
+		character.setY(newPos);
+	}
+	else if(key == KeyEvent.VK_LEFT) {
+		//shoot
+	}
+	else if(key == KeyEvent.VK_RIGHT) {
+		//shoot
+	}
+	else if(key == KeyEvent.VK_UP) {
+		//shoot
+	}
+	else if(key == KeyEvent.VK_DOWN) {
+		//shoot             
+	}
+	}
+
+
 
 }
