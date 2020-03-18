4e38bc598012bf33bccd0a1ee3f7afa2578c1a59
nta37 <nta37@sfu.ca>
2020-03-17 18:02:30 -0700

    adding javadocs to BoardEntity.java

--- Escape/src/main/java/com/Group12/Game/BoardEntity.java
+++ Escape/src/main/java/com/Group12/Game/BoardEntity.java
@@ -1,9 +1,32 @@
 package com.Group12.Game;
 
+/**
+ * A representation of an entity on a coordinate plane.
+ * @author Yuxi
+ *
+ */
 interface BoardEntity {
+	/**
+	 * Returns the entity's x coordinate.
+	 * @return the x coordinate
+	 */
 	public int getXPos();
+	
+	/**
+	 * Returns the entity's y coordinate.
+	 * @return the y coordinate
+	 */
 	public int getYPos();
 	
+	/**
+	 * Sets the entity's x coordinate.
+	 * @param x the new x coordinate
+	 */
 	public void setX(int x);
+	
+	/**
+	 * Sets the entity's y coordinate.
+	 * @param y the new y coordinate
+	 */
 	public void setY(int y);
 }
