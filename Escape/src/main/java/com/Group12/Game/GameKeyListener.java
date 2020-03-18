2be76ef2b84530b058dc9c0e0803da82ec96cceb
Wilson <wwa87@sfu.ca>
2020-03-18 00:28:37 -0700

    Now obtains and saves lastkey

--- Escape/src/main/java/com/Group12/Game/GameKeyListener.java
+++ Escape/src/main/java/com/Group12/Game/GameKeyListener.java
@@ -3,88 +3,73 @@ package com.Group12.Game;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+
 public class GameKeyListener implements KeyListener {
-	public void keyTyped(KeyEvent e) {
-		// Don't need this
+
+	private int lastKey;
+	public void keyPressed(KeyEvent e) {
+
+		lastKey = e.getKeyCode();
+
 	}
 	
-	public void keyPressed(KeyEvent e) {
-		int key = e.getKeyCode();
-		int newPos;
-		
-		switch(key) {
-			
-			//up
-			case KeyEvent.VK_W:
-				if(state==GameMain.GameState.GAME) {
-					newPos = character.getXPos() + 1;
-					character.setXPos(newPos);
-					System.out.println(newPos);
-					break;
-				}
+	public int getLastKey() {
+		return lastKey;
+	}
+	
+	public void resetLastKey() {
+		lastKey = 0;
+	}
 			
 			//left
-			case KeyEvent.VK_A:
-				if(state==GameMain.GameState.GAME) {
-					newPos = character.getYPos() - 1;
-					character.setYPos(newPos);
-					System.out.println(newPos);
-					break;
-				}
+			//case KeyEvent.VK_A:
+
 			//down
-			case KeyEvent.VK_S: 
-				if(state==GameMain.GameState.GAME) {
-					newPos = character.getXPos() - 1;
-					character.setXPos(newPos);
-					System.out.println(newPos);
-					break;
-				}
+			//case KeyEvent.VK_S: 
+
 			//right
-			case KeyEvent.VK_D :
-				if(state==GameMain.GameState.GAME) {
-					newPos = character.getYPos() + 1;
-					character.setYPos(newPos);
-					System.out.println(newPos);
-					break;
-				}
-				
-				else {
-					break;
-				}
+			//case KeyEvent.VK_D :
 
 			
 			//Change from Menu State to Game State pressing ENTER KEY
 			//If state is already in menu, do nothing
-			case KeyEvent.VK_ENTER: 
-				if(state == GameMain.GameState.GAME) {
-					break;
-				}
-				else {
-					state = GameMain.GameState.GAME;
-					updateState.setState(state);
-					repaint();
+			//case KeyEvent.VK_ENTER: 
+				//if(state == GameMain.GameState.GAME) {
+				//	break;
+				//}
+				//else {
+				//	state = GameMain.GameState.GAME;
+				//	updateState.setState(state);
+				//	repaint();
 					
-				}
-			break;
-		
+				
+
 			//Change from Game State to Menu State by pressing ESC KEY
 			//If state is already in Game, do nothing
-			case KeyEvent.VK_ESCAPE:
-				if(state == GameMain.GameState.MENU) {
-					break;
-				}
-				else {
-					state = GameMain.GameState.MENU;
-					updateState.setState(state);	
-					repaint();
-				}
-				break;
-		}
-	}
+			//case KeyEvent.VK_ESCAPE:
+				//if(state == GameMain.GameState.MENU) {
+				//	break;
+				//}
+				//else {
+				//	state = GameMain.GameState.MENU;
+				//	updateState.setState(state);	
+				//	repaint();
+				//}
+				//break;
+		
+
 	
 
 	public void keyReleased(KeyEvent e) {
 		// Don't need this
+	}
+
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
 
