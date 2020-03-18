2f73ea1ddc242c1bc69fc5894dd0f3ec0c3e6d81
Wilson <wwa87@sfu.ca>
2020-03-18 02:32:13 -0700

    Test

--- Escape/src/main/java/com/Group12/Game/GameKeyListener.java
+++ Escape/src/main/java/com/Group12/Game/GameKeyListener.java
@@ -5,61 +5,6 @@ import java.awt.event.KeyListener;
 
 
 public class GameKeyListener implements KeyListener {
-<<<<<<< HEAD
-
-	private int lastKey;
-	public void keyPressed(KeyEvent e) {
-
-		lastKey = e.getKeyCode();
-
-	}
-	
-	public int getLastKey() {
-		return lastKey;
-	}
-	
-	public void resetLastKey() {
-		lastKey = 0;
-	}
-			
-			//left
-			//case KeyEvent.VK_A:
-
-			//down
-			//case KeyEvent.VK_S: 
-
-			//right
-			//case KeyEvent.VK_D :
-
-			
-			//Change from Menu State to Game State pressing ENTER KEY
-			//If state is already in menu, do nothing
-			//case KeyEvent.VK_ENTER: 
-				//if(state == GameMain.GameState.GAME) {
-				//	break;
-				//}
-				//else {
-				//	state = GameMain.GameState.GAME;
-				//	updateState.setState(state);
-				//	repaint();
-					
-				
-
-			//Change from Game State to Menu State by pressing ESC KEY
-			//If state is already in Game, do nothing
-			//case KeyEvent.VK_ESCAPE:
-				//if(state == GameMain.GameState.MENU) {
-				//	break;
-				//}
-				//else {
-				//	state = GameMain.GameState.MENU;
-				//	updateState.setState(state);	
-				//	repaint();
-				//}
-				//break;
-		
-
-=======
 	
     private int lastKey;
 
@@ -78,19 +23,10 @@ public class GameKeyListener implements KeyListener {
 	public void keyPressed(KeyEvent e) {
         lastKey = e.getKeyCode();
     }
->>>>>>> 888d1680b625bc0820ff078ae77a0d37e3760343
 	
 
 	public void keyReleased(KeyEvent e) {
 		// Don't need this
-	}
-
-
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 }
 
