376f28cf0b687c762a4f9d58da5f3bb30643040c
Wilson <wwa87@sfu.ca>
2020-03-17 21:42:40 -0700

    No changes

--- Escape/src/main/java/com/Group12/Game/BoardEntity.java
+++ Escape/src/main/java/com/Group12/Game/BoardEntity.java
@@ -4,20 +4,12 @@ package com.Group12.Game;
 public class BoardEntity {	
 	
 	//X and Y position for Main Character
-<<<<<<< HEAD
 
-=======
->>>>>>> e3114afb42d822e456117226845528c956d1d894
 	private int charXpos;
 	private int charYpos;
 	
 	
-<<<<<<< HEAD
-=======
-	
-	
-	
->>>>>>> e3114afb42d822e456117226845528c956d1d894
+
 	public int getXPos() {
 		return charXpos;
 	
