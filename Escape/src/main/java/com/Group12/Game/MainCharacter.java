4f152cdc017bd0e78fbd8ff426a9acc32f0b6e9c
Wilson <wwa87@sfu.ca>
2020-03-17 19:19:21 -0700

    Clean up

--- Escape/src/main/java/com/Group12/Game/MainCharacter.java
+++ Escape/src/main/java/com/Group12/Game/MainCharacter.java
@@ -1,14 +1,9 @@
 package com.Group12.Game;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyAdapter;
 public class MainCharacter {
 	
-	
-	private int x;
-	private int y;
 	private int health = 100;
+	
 	//private Weapon currentWeapon;
 	
 	public int getHealth() {
@@ -32,61 +27,7 @@ public class MainCharacter {
 		}
 	}
 	
-	public int getXPos() {
-		return x;
-	
-	}
-	
-	public int getYPos() {
-		return y;
-	}
-	
-	public void setX(int x) {
-		this.x = x;
-	}
-	
-	public void setY(int y) {
-		this.y = y;
-	}
-	
-	public void keyPressed(KeyEvent e) {
-		int key = e.getKeyCode();
-		int newPos;
-		
-	
-		//up
-		if(key == KeyEvent.VK_W) {
-			newPos = getXPos() + 1;
-			setX(newPos);
-			
-		}
-		//left
-		else if(key == KeyEvent.VK_A) {
-			newPos = getYPos() - 1;
-			setY(newPos);
-		}
-		//down
-		else if(key == KeyEvent.VK_S) {
-			newPos = getXPos() - 1;
-			setX(newPos);
-		}//right
-		else if(key == KeyEvent.VK_D) {
-			newPos = getYPos() + 1;
-			setY(newPos);
-		}
-		else if(key == KeyEvent.VK_LEFT) {
-			//shoot
-		}
-		else if(key == KeyEvent.VK_RIGHT) {
-			//shoot
-		}
-		else if(key == KeyEvent.VK_UP) {
-			//shoot
-		}
-		else if(key == KeyEvent.VK_DOWN) {
-			//shoot             
-		}
-	}	
+
 
 	
 	public static void main (String args[]) {
