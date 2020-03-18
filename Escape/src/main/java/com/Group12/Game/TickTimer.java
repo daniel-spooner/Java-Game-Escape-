b2da84cb45d7312c4b74e7880a0e8d6403452180
nta37 <nta37@sfu.ca>
2020-03-17 23:50:12 -0700

    javadocs; addition of vital game logic function call

--- Escape/src/main/java/com/Group12/Game/TickTimer.java
+++ Escape/src/main/java/com/Group12/Game/TickTimer.java
@@ -1,74 +1,95 @@
 package com.Group12.Game;
 
-
+/**
+ * Runs GameMain.update() some number of times per second,
+ * and keeps track of how many milliseconds have passed since beginning execution.
+ * @author Daniel
+ *
+ */
 public class TickTimer implements Runnable {
 
 	private Boolean tickActive;
-	private int FPS;
+	private int fps;
 	private int delay;
 	private long tickCount;
 	// private long maxCount;
-   
+	private GameMain game;
+	
+	/** 
+	 * Default constructor.
+	 */
 	public TickTimer() {
 		this(30);
 	}
-
-	public TickTimer(int FPS) {
-		this.FPS = FPS;
-		this.delay = 1000/FPS;
+	
+	/**
+	 * Creates a TickTimer with a number of function calls per second.
+	 * @param fps "function-calls" per second 
+	 */
+	public TickTimer(int fps) {
+		this.fps = fps;
+		this.delay = 1000/fps;
+		this.game = GameMain.getInstance();
 	}
  
 	private void runTick() {
 		if (tickActive) {
 			
-			// CALL the update function here
+			game.update();
+			
 			try {
 				Thread.sleep(delay);
 			} catch (Exception e) {
 				System.out.println(e);
 			}
-			
 			tickCount += delay;
-			
-			/*
-			System.out.println(tickCount);
-			if (tickCount > 5000) {
-				pauseTick();
-			}
-			*/
 			
 			runTick();
 		}
 	}
 	
+	/**
+	 * Pauses the TickTimer, causing it to stop running after its next loop.
+	 */
 	public void pauseTick() {
 		tickActive = false;
 	}
 	
+	/**
+	 * Unpauses the TickTimer, causing it to continue running after being paused.
+	 * Behaviour undefined if used when the TickTimer is not currently paused.
+	 */
 	public void unpauseTick() {
 		tickActive = true;
 		runTick();
 	}
 	
+	/**
+	 * Gets the current count of milliseconds since reset.
+	 * @return milliseconds since reset
+	 */
 	public long getTickCount() {
 		return tickCount;
 	}
 	
+	/**
+	 * Resets the current tracker of milliseconds.
+	 */
 	public void resetTickCount() {
 		tickCount = 0;
 	}
    
-	
+	/**
+	 * Starts the main execution loop of TickTimer.
+	 */
 	public void run() {
 		tickCount = 0;
 		tickActive = true;
 		
 		runTick();
 	}
-   
-   
+	
 	public static void main(String[] args) throws Exception {
-      
 		Thread t = new Thread(new TickTimer());
 		t.start();
 
