d337977121a41fc9ea64aacd4e71ad33c9e9223d
Wilson <wwa87@sfu.ca>
2020-04-06 16:03:26 -0700

    added comments to explain the boundaries of junit tests for maincharacter's health.

--- Escape/src/test/java/com/Group12/Game/MainCharacterTest.java
+++ Escape/src/test/java/com/Group12/Game/MainCharacterTest.java
@@ -6,13 +6,14 @@ import org.junit.Test;
 
 public class MainCharacterTest {
 
-	
+	//Checks if Initial health is set to 100
 	@Test
 	public void	initialHealth() {
 		MainCharacter test = new MainCharacter();
 		int output = test.getHealth();
 		assertEquals(100,output);
 	}
+	//
 	@Test
 	public void decreaseHealthByOne() {
 		MainCharacter test = new MainCharacter();
@@ -29,6 +30,17 @@ public class MainCharacterTest {
 			output = test.decHealth();
 		}
 		assertEquals(false,output);
+		
+	}	
+	@Test
+	public void decreaseHealthTo99() {
+		MainCharacter test = new MainCharacter();
+		boolean output = true;
+		//Decrease the health 100 times from initial 100 health.
+		for(int i = 0; i<99; i++) {
+			output = test.decHealth();
+		}
+		assertEquals(true,output);
 		
 	}
 	@Test
