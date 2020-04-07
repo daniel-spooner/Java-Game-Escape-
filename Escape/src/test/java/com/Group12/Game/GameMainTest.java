91249d18596b38dc0acf3c57d953e7b0bb41b255
nta37 <nta37@sfu.ca>
2020-04-06 22:37:16 -0700

    GameMainTest, getScore() getInstance() and startGame() tested

--- Escape/src/test/java/com/Group12/Game/GameMainTest.java
+++ Escape/src/test/java/com/Group12/Game/GameMainTest.java
@@ -1,35 +1,34 @@
 package com.Group12.Game;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class GameMainTest {
 	
 	GameMain gm = GameMain.getInstance();
+	TickTimer tick;
+	
+	@Test
+	public void testingGetScore() {
+		assertEquals(0, gm.getScore());
+	}
 	
 	@Test
 	public void testingGetInstance() {
-		
-	}
-
-	@Test
-	public void testingSetState() {
-		
-	}
-
-	@Test
-	public void testingGetScore() {
-		
+		assertEquals(0, gm.getScore());
+		GameMain gm2 = GameMain.getInstance();
+		assertTrue(gm2 == gm);	// pointer match, not value
 	}
 
 	@Test
 	public void testingStartGame() {
-		
+		gm.startGame();
+		assertEquals(500, gm.getScore());
 	}
 	
-	@Test
-	public void testingSetTickTimer() {
-		
-	}
+	// Integration test
 	
 	@Test
 	public void testingUpdate() {
