bc86dd8b70ab1701ab3c11d8824fd081ba588dae
Daniel Spooner <daniel_spooner@sfu.ca>
2020-04-06 21:16:38 -0700

    Added all unit tests for Cell class

---
+++ Escape/src/test/java/com/Group12/Game/CellTest.java
@@ -0,0 +1,32 @@
+package com.Group12.Game;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CellTest {
+	
+	Cell cell;
+	
+	@Before
+	public void setup() {
+		cell = new Cell();
+	}
+	
+	@Test
+	public void testingCellConstrucor() {
+		cell = new Cell(cellType.OPEN);
+		cellType result = cell.getType();
+		assertEquals(cellType.OPEN, result);
+	}
+	
+	@Test
+	public void testingGetType() {
+		cellType result = cell.getType();
+		assertEquals(cellType.OPEN, result);
+	}
+	
+}
