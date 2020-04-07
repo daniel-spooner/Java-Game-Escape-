15eefad2fff79368f7e7f52e30ca017d16c5b140
nta37 <nta37@sfu.ca>
2020-04-06 20:37:47 -0700

    BoardEntityTest class outline

---
+++ Escape/src/test/java/com/Group12/Game/BoardEntityTest.java
@@ -0,0 +1,49 @@
+package com.Group12.Game;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardEntityTest {
+	
+	BoardEntity be;
+	int x = 5;
+	int y = 10;
+	
+	@Before
+	public void setupTests() {
+		be = new BoardEntity(x, y);
+	}
+	
+	@Test
+	public void testingGetXPos() {
+		
+	}
+	
+	@Test
+	public void testingGetYPos() {
+		
+	}
+	
+	@Test
+	public void testingDefaultConstructor() {
+		
+	}
+	
+	@Test
+	public void testingRegularConstructor() {
+		
+	}
+	
+	@Test
+	public void testingSetXPos() {
+		
+	}
+	
+	@Test 
+	public void testingSetYPos() {
+		
+	}
+	
+}
