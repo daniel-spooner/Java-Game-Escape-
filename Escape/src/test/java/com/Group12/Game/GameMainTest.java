1b96473ee5c299043c8f87feddff5999c071d418
nta37 <nta37@sfu.ca>
2020-04-06 21:24:24 -0700

    GameMainTest initialization and outline of all public functions

---
+++ Escape/src/test/java/com/Group12/Game/GameMainTest.java
@@ -0,0 +1,39 @@
+package com.Group12.Game;
+
+import org.junit.Test;
+
+public class GameMainTest {
+	
+	GameMain gm = GameMain.getInstance();
+	
+	@Test
+	public void testingGetInstance() {
+		
+	}
+
+	@Test
+	public void testingSetState() {
+		
+	}
+
+	@Test
+	public void testingGetScore() {
+		
+	}
+
+	@Test
+	public void testingStartGame() {
+		
+	}
+	
+	@Test
+	public void testingSetTickTimer() {
+		
+	}
+	
+	@Test
+	public void testingUpdate() {
+		
+	}
+	
+}
